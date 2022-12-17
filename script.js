95bd5a6f154b34f14e5755e16cc325e53dd63b71
AryaHuda <56615453+AryaHuda@users.noreply.github.com>
2022-12-18 00:10:55 +0800

    js

--- script.js
+++ script.js
@@ -1,62 +1,25 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+let navbar = document.querySelector('.header .navbar');
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
-
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
-});
-
-
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
-
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
-
-
-const searchButton = document.querySelector('#content nav form .form-input button');
-const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
-
-searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
-		e.preventDefault();
-		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
-			searchButtonIcon.classList.replace('bx-search', 'bx-x');
-		} else {
-			searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		}
-	}
-})
-
-
-if(window.innerWidth < 768) {
-	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
-	searchButtonIcon.classList.replace('bx-x', 'bx-search');
-	searchForm.classList.remove('show');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
 }
 
-window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
-		searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		searchForm.classList.remove('show');
-	}
-})
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
 
-const switchMode = document.getElementById('switch-mode');
+let mainVid = document.querySelector('.main-video');
 
-switchMode.addEventListener('change', function () {
-	if(this.checked) {
-		document.body.classList.add('dark');
-	} else {
-		document.body.classList.remove('dark');
-	}
-})
+document.querySelectorAll('.course-3 .box .video video').forEach(vid =>{
+
+    vid.onclick = () =>{
+        let src = vid.getAttribute('src');
+        mainVid.classList.add('active');
+        mainVid.querySelector('video').src = src;
+    }
+
+});
+
+document.querySelector('#close-vid').onclick = () =>{
+    mainVid.classList.remove('active');
+}
